75b2214e5065f64cb88bc1079da3ba6c3ed866d3
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2026-01-22 16:45:05 +0700

    Add order management functionality with routes, controllers, and services

--- src/routes/menus.route.js
+++ src/routes/menus.route.js
@@ -4,9 +4,9 @@ import { authenticate, authorize } from "../middlewares/auth.middleware.js";
 import express from "express";
 const router = express.Router();
 
-router.post("/api/menus", authenticate, authorize(["admin"]), upload.single("image"), addMenu);
-router.get("/api/menus", getMenus);
-router.put("/api/menus/:id_menu", authenticate, authorize(["admin"]), upload.single("image"), updateMenu);
-router.delete("/api/menus/:id_menu", authenticate, authorize(["admin"]), deleteMenu);
+router.post("/menus", authenticate, authorize(["admin"]), upload.single("image"), addMenu);
+router.get("/menus", getMenus);
+router.put("/menus/:id_menu", authenticate, authorize(["admin"]), upload.single("image"), updateMenu);
+router.delete("/menus/:id_menu", authenticate, authorize(["admin"]), deleteMenu);
 
 export default router;
