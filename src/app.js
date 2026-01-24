93bd28ea7e67216503b052d24a68dee38416d1dd
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2026-01-24 16:02:36 +0700

    fix: restrict CORS origin to localhost for improved security

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
