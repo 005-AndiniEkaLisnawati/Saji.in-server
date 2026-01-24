78f6e5d82189e20f138bf3cddc915642461b899b
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2026-01-24 16:04:29 +0700

    fix

--- src/routes/orders.route.js
+++ src/routes/orders.route.js
@@ -19,7 +19,7 @@ router.put("/orders/:id_order", editOrder);
 router.delete("/orders/:id_order", deleteOne);
 router.get("/orders/:id_order", getOrder);
 router.patch("/orders/:id_order/status", updateOrderStatus);
-router.get("/barista/queue", authenticate, authorize(["barista", "admin"]), getBaristaQueue);
+router.get("/barista/queue", getBaristaQueue);
 
 
 export default router;
