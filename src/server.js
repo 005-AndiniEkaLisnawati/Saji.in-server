3d4dbb1e494d6edd540176d7b2875b4d74181b73
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2026-02-09 10:36:45 +0700

    add promos endpoint

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import app from './app.js';
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+
 });
+
+
 
 export default app;
